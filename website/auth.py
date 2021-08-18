b7611b28530a75c4f50b55ba73d8d0c81a16932b
richie <richievvvhuynh@gmail.com>
2021-08-18 12:14:37 -0700

    created templates for routes and working on datebase creating schemasfor User and Notes

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,38 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif len(last_name) < 2:
+            flash('Last name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
+    return render_template("sign_up.html")
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+   return render_template("login.html", boolean=True)
 
 
 @auth.route('/logout')
 def logout():
-    return "<p>logout</p>"
+    return "<p>You have successfully logged out :)</p>"
 
 
-@auth.route('/sign-up')
-def sign_up():
-    return "<p>Sign up</p>"
